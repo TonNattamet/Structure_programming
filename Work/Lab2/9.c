fde6b92b7551ed7f2f914c3d17c886cdd876328e
Nattamet <102680145+TonNattamet@users.noreply.github.com>
2023-01-04 13:31:46 +0700

    Update 9.c

--- Work/Lab2/9.c
+++ Work/Lab2/9.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(){
+int main()
+{
+    int A = 0, B = 0, M = 0, N = 0;
+    scanf("%d %d %d %d", &A, &B, &M, &N);
+    if ((A == 0 && > 0) || (B == 0 & 1 > 0))
+    {
+        printf("Unable to complete order");
+    }
+    else
+    {
+        r1 = celi( M*1.0/(A*6.0));
+    }
+    if(M==0 && N==0)
+    {
+        r2 = 0;
+    }
+    else
+    {
+        r2 = celi( N*1.0/(B*10.0));
+    }
+    if(r1 > r2)
+    {
+        printf("%d",r1);
+    }
+    else
+    {
+        printf("%d",r2);
+    }
     
 
     return 0;
