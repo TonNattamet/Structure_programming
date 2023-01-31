18954a52bcd95c8b14b5a9f0e474227b66143b12
Nattamet <102680145+TonNattamet@users.noreply.github.com>
2023-01-31 20:36:11 +0700

    debug

--- test/test.c
+++ test/test.c
@@ -2,13 +2,11 @@
 #include <stdlib.h>
 int main()
 {
-float cherprang, music, jameji ;
-cherprang = 70;
-/* data declaration */
-/* process */
-music = cherprang - cherprang *20/100;
-jameji = (cherprang + music )/2;
-printf("cherprang = %6.2f, music = %6.2f, jameji = %6.2f\n", cherprang, music , jameji);
-system("PAUSE");
+  for(int i=1;i<=12;i++){
+    for(int j=9;j<=12;j++){
+        printf("%5d x %d = %d",j,i,j*i);
+    }
+    printf("\n");
+  }
 return 0;
 }
