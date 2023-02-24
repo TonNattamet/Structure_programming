8db84ce874fb8fdd38f606c930c86c191110259b
Nattamet <102680145+TonNattamet@users.noreply.github.com>
2023-02-24 17:24:32 +0700

    testmid

--- Work/Mid/Lab6/labGrader_6_3.c
+++ Work/Mid/Lab6/labGrader_6_3.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    int m,n,i,j,lp=0;
-    scanf("%d %d",&m,&n);
-    int A[m][n],B[n][m];
-    for (i=0;i<m;i++){
-        for(j=0;j<n;j++){
-            scanf("%d",&A[i][j]);
+int main()
+{
+    int m, n, i, j, lp = 0;
+    scanf("%d %d", &m, &n);
+    int A[m][n], B[n][m];
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            scanf("%d", &A[i][j]);
         }
     }
-    for (i=0;i<m;i++){
-        for(j=0;j<n;j++){
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
             B[i][j] = A[j][i];
         }
     }
-    for (i=0;i<m;i++){
-        for(j=0;j<n;j++){
-            printf("%d ",B[i][j]);
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%d ", B[i][j]);
         }
         printf("\n");
     }
